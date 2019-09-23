ae4ce37effe2cdfbe8c573037686fac124d3ccff
Jesse Smith <jesse.lee.smith@gmail.com>
2019-09-23 20:25:35 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -5,13 +5,15 @@ def find_element_index(array, value_to_find)
       return index
     end
   end
-  
+  nil
 end
 
 def find_max_value(array)
-  # Add your solution here
+  array.sort!
+  array.pop
 end
 
 def find_min_value(array)
-  # Add your solution here
+  array.sort!
+  array.shift
 end
